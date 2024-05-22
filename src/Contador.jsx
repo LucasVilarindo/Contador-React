b81ce8f93e9ef1f60cfc6827ad87cc815b06f190
LucasVilarindo <lucasvilarindo@outlook.com>
2024-05-22 19:12:58 -0300

    first commit

---
+++ src/Contador.jsx
@@ -0,0 +1,49 @@
+
+import './Contador.css'
+import React, { useState } from 'react'; 
+
+  
+
+function Contador() { 
+
+  const [contador, setContador] = useState(0); 
+
+  
+
+  const incrementar = () => { 
+
+    setContador(contador + 1); 
+
+  }; 
+
+  
+
+  const decrementar = () => { 
+
+    setContador(contador - 1); 
+
+  }; 
+
+  
+
+  return ( 
+
+    <div> 
+
+      <h2>Contador</h2> 
+
+      <p>Valor: {contador}</p> 
+
+      <button onClick={incrementar}>Inserir</button> 
+
+      <button onClick={decrementar}>Retirar</button> 
+
+    </div> 
+
+  ); 
+
+} 
+
+  
+
+export default Contador; 
